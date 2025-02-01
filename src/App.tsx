af8b8a4eb27c472627f7fe3ab6ec199d8d6a11cc
yiningxiang <corrine.xiang@mail.utoronto.ca>
2025-02-01 12:41:31 -0500

    Resolve workflow issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { useState } from 'react'
 import Background from './components/Background'
 import Navbar from './components/layout/Navbar'
 import Hero from './components/Hero'
@@ -7,15 +5,13 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 function App() {
-  const [theme, setTheme] = useState('dark')
-
   return (
     <div className="w-full min-h-screen relative bg-black">
       <div className="absolute inset-0">
         <Background />
       </div>
       <div className="relative z-10">
-        <Navbar theme={theme} setTheme={setTheme} />
+        <Navbar />
         <main className="container mx-auto px-4">
           <Hero />
           <Projects />
--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { FiSun, FiMoon } from 'react-icons/fi'
